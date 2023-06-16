01108c72851449b6c77a05bdd47754e1589f45e9
kathySagredo <136348407+kathySagredo@users.noreply.github.com>
2023-06-16 15:03:15 -0400

    Delete CrearObj.js

--- EjerciciosObjetos/CrearObj.js
+++
@@ -1,9 +0,0 @@
-const primerObjeto = {
-    nombre:"Kathy",
-    apellido:"Sagredo",
-    edad:25,
-    email:"kathy.sagredo@usach.cl",
-    direccion:"René Silva Espejo 66"
-}
-
-console.table(primerObjeto)
