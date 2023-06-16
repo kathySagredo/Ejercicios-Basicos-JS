48b1e37caaf3229a0a9cce39cca48fb92916f104
kathySagredo <136348407+kathySagredo@users.noreply.github.com>
2023-06-16 15:02:28 -0400

    Add files via upload

---
+++ EjerciciosObjetos/Objetos/CrearObj.js
@@ -0,0 +1,9 @@
+const primerObjeto = {
+    nombre:"Kathy",
+    apellido:"Sagredo",
+    edad:25,
+    email:"kathy.sagredo@usach.cl",
+    direccion:"René Silva Espejo 66"
+}
+
+console.table(primerObjeto)
