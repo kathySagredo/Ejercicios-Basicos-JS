72d9418f189716a2112319c42fb0a5d46cfbc690
kathySagredo <kathy.sagredo@usach.cl>
2023-06-28 12:07:37 -0400

    Actualización de cambios

--- Challenge grupal_Lista Tareas/ListaTarea.js
+++ Challenge grupal_Lista Tareas/ListaTarea.js
@@ -9,19 +9,6 @@ const nuevaOl = document.createElement("ol");
 const nuevalista = lista.appendChild(nuevaOl);
 
 
-
-/* function agregarLista(){
-    let nombreTarea = form.querySelector("input[id='tarea']").value
-    console.log(nombreTarea)
-    const nuevaLi = document.createElement("li");
-    btnAñadir.addEventListener('click',(e)=>{
-        e.preventDefault();
-        console.log(e)
-        nuevaLi.textContent = nombreTarea; 
-        nuevalista.appendChild(nuevaLi);
-    }) 
-} */
-
 function agregarLista(){
 
     btnAñadir.addEventListener('click',(e)=>{
